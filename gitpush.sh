6f0cca50b024296180e2f02c5d614fa61ff0220b
Julio Makdisse Saito <saitodisse@gmail.com>
2014-05-13 13:30:37 -0300

    "$1"

---
+++ gitpush.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+echo "root:"
+git add . -A; git commit -m'"$1"'; git push origin
+echo ""
+
+echo "1-player:"
+(cd 1-player && git add . -A; git commit -m'"$1"'; git push origin)
+echo ""
+
+echo "2-elastic-mp3-searcher:"
+(cd 2-elastic-mp3-searcher && git add . -A; git commit -m'"$1"'; git push origin)
+echo ""
+
+echo "3-socketServer:"
+(cd 3-socketServer && git add . -A; git commit -m'"$1"'; git push origin)
+echo ""
+
+echo "4-mp3Server:"
+(cd 4-mp3Server && git add . -A; git commit -m'"$1"'; git push origin)
+echo ""
+
