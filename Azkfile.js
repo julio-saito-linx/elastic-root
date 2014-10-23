65d6af99cec6394a427256000e97dd426179f73e
Julio Makdisse Saito <saitodisse@gmail.com>
2014-10-23 21:50:50 -0200

    making work with AZK

--- Azkfile.js
+++ Azkfile.js
@@ -18,7 +18,7 @@ systems({
     workdir: "/azk/#{manifest.dir}/1-player",
     command: "node_modules/grunt-cli/bin/grunt serve",
     mounts: {
-      '/azk/#{manifest.dir}': path("."),
+      '/azk/#{manifest.dir}': path(".")
     },
     scalable: {"default": 1},
     http: {
@@ -28,7 +28,11 @@ systems({
     envs: {
       // set instances variables
       NODE_ENV: "development",
+      // LIVE_RELOAD_PORT: "35731"
     },
+    ports:{
+      portLiveReload: "35731:35731/tcp",
+    }
   },
   'searcher': {
     // Dependent systems
@@ -54,7 +58,11 @@ systems({
     envs: {
       // set instances variables
       NODE_ENV: "development",
+      // LIVE_RELOAD_PORT: "35732"
     },
+    ports:{
+      portLiveReload: "35732:35732/tcp",
+    }
   },
   'socketserver': {
     // Dependent systems
@@ -94,7 +102,10 @@ systems({
     workdir: "/azk/#{manifest.dir}/4-mp3Server",
     command: "node app.js",
     mounts: {
-      '/azk/#{manifest.dir}': path("."),
+      '/azk/#{manifest.dir}'           : path('.'),
+      '/azk/mounts/media/julio/Mp3/'   : path('/media/julio/Mp3'),
+      '/azk/mounts/media/julio/2gb/'   : path('/media/julio/2gb'),
+      '/azk/mounts/media/julio/Files/' : path('/media/julio/Files'),
     },
     scalable: {"default": 1},
     http: {
@@ -106,15 +117,9 @@ systems({
       NODE_ENV: "development"
     },
   },
+
   'elastic-database': {
-    // Dependent systems
-    depends: [],
-    // More images:  http://images.azk.io
     image: "dockerfile/elasticsearch",
-    // Steps to execute before running instances
-    // provision: [
-    //   "echo > elasticsearch.yml",
-    // ],
     workdir: "/azk/#{manifest.dir}/elastic-database",
     command: "/elasticsearch/bin/elasticsearch -Des.config=/azk/#{manifest.dir}/elastic-database/elasticsearch.yml",
     mounts: {
@@ -124,15 +129,8 @@ systems({
       portA: "9200:9200/tcp",
       portB: "9300:9300/tcp",
     }
-    // scalable: {"default": 1},
-    // http: {
-    //   hostname: "#{system.name}.#{azk.default_domain}"
-    // },
-    // envs: {
-    //   // set instances variables
-    //   NODE_ENV: "development"
-    // },
   },
+
 });
 
 
